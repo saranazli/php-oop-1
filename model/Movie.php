cb8205605916b07c2ddb3200ff37eb7437422a30
s4r44d43 <sararez@live.it>
2024-05-02 14:04:36 +0200

    [setted] class

--- model/Movie.php
+++ model/Movie.php
@@ -1,5 +1,34 @@
 <?php 
 
+class Movie{
 
+  private $title;
+  private $subtitle;
+
+  public $mainActor;
+  public $director;
+
+  public function __construct( string $_title, string $_subtitle){
+
+    $this -> title = $_title;
+    $this -> subtitle = $_subtitle;
+
+  }
+
+  public function setName(){
+
+  }
+  public function setSub(){
+
+  }
+
+  public function getName(){
+
+  }
+  public function getSub(){
+
+  }
+  
+}
 
 ?>
